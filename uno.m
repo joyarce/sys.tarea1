83369a68fdfed8a41a637104410cd8ce01e64fcc
joyarce <39476709+joyarce@users.noreply.github.com>
2020-10-19 12:15:44 -0300

    Update uno.m

--- uno.m
+++ uno.m
@@ -1,23 +1,54 @@
 close all; clear all; clc;
-format long g
 
 % Sonido Original
-[x,Fs] = audioread('acoustic.wav'); % input archivo .wav
-TotalTime = length(x)./Fs; % Tiempo durac�on input .wav [s] 
-n=input('Numero de ecos ');
+a = 0.5; % Amplitud eco uniforme
+info = audioinfo("acoustic.wav");
+[y,Fs] = audioread("acoustic.wav"); % input archivo .wav
+largo = size(y);
 
+
+
+n=input('Numero de ecos ');
 for i=1:n
 msge = sprintf('Ingresar retardo eco numero %d    :',i)
 retardos(i)=input(msge);
 end
+
+subplot(2,2,1);
+plot(y)
+ylabel("Amplitud");
+title("Señal original");
+
 for i=1:n
-cerosadd(i) = round((TotalTime+retardos(i))*Fs-length(x))
+    subplot(2,2,i+1)
+    plot(echo_gen(y,Fs,retardos(i),a));
+    ylabel("Amplitud");
+    title("Señal eco");
 end
 
 
-for i=1:n
-c = zeros(cerosadd(i),1)
-subplot(2,2,i)
-plot([c;x/2])
-
+function output = echo_gen(input, fs, delay, amp)
+ds = round((delay)*fs);    %%CALCULATING DELAY SAMPLE NUMBERS
+if ds==0                   %%THIS APPENDS TO THE INPUT TO EQUAL THE SIZE WITH OUTPUT
+    append=[];             %%APPENDS EMPTY MATRIX WHILE 'ds' IS ZERO
+else
+    append=zeros(ds,1);    %%APPENDS ZERO VECTOR FOR THE DELAY
+end
+   ain = [append;input];   %%APPENDED INPUT
+    dmwa = amp*ain;        %%AMPLIFIED SIGNAL
+    outd = [input; append];  %%APPENDED OUTPUT
+    out = (dmwa+outd);      %%OUTPUT WITHOUT SCALLING
+    
+%%% SCALLING (IF NEEDED)   
+    mx = max(out);
+    mn = min(out);
+    if max(abs(out))>1
+        if(abs(mx)>abs(mn))
+            output = out/abs(mx);
+        else
+            output = out/abs(mn);
+        end
+    else
+        output=out;          %%OUTPUT WITH SCALLING(IF NEEDED)
+    end
 end
